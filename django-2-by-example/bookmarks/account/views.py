286ba48d0a81ee6cde660d568bc7cc2f373d6527
ohduran <alvaro.duranb@gmail.com>
2018-11-15 09:55:39 +0100

    django2 - Building a social website: creating a login view

--- django-2-by-example/bookmarks/account/views.py
+++ django-2-by-example/bookmarks/account/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Authenticate checks user credentials and returns a User
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    # login sets the user in the current session
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
