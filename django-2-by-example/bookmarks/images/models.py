9a97496f1d83c529dce33a3b7159d1d4d584b493
ohduran <alvaro.duranb@gmail.com>
2018-11-15 16:41:52 +0100

    django2 - Building a social website: Images

--- django-2-by-example/bookmarks/images/models.py
+++ django-2-by-example/bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
@@ -22,6 +23,9 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
