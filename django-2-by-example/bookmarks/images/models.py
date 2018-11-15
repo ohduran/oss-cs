ad3294a02c8a8fa064bff85d95e1f3d073741a3d
ohduran <alvaro.duranb@gmail.com>
2018-11-15 15:34:18 +0100

    django2 - Building a social website: Creating many-to-many relationships

--- django-2-by-example/bookmarks/images/models.py
+++ django-2-by-example/bookmarks/images/models.py
@@ -16,6 +16,10 @@ class Image(models.Model):
     created = models.DateField(auto_now_add=True,
                                db_index=True)
 
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
+
     def __str__(self):
         return self.title
 
