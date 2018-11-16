7bfc5ea1db47c4a508d6c9d2ab6130caa185d69e
ohduran <alvaro.duranb@gmail.com>
2018-11-16 09:39:22 +0100

    django2- Build a social website: Adding AJAX

--- django-2-by-example/bookmarks/images/views.py
+++ django-2-by-example/bookmarks/images/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
@@ -30,3 +32,21 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action in ['like', 'dislike']:
+        try:
+            image = get_object_or_404(Image, id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
